2cce26daa1613e6a454e170aa0623c833f9cd364
Nuri Kim <kimn8684@gmail.com>
2019-03-24 22:21:40 -0700

    grocery list manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { GroceryComponent } from './app.grocery'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GroceryComponent]
 })
 export class AppModule { }
